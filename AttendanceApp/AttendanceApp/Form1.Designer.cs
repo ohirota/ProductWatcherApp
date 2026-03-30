75c181179c4538184f9efe326617723764d5dab7
ohirota <ohirota2220@gmail.com>
2026-03-30 20:17:26 +0900

    打刻アプリ完全版　表示形式とボタン制御完了

--- AttendanceApp/AttendanceApp/Form1.Designer.cs
+++ AttendanceApp/AttendanceApp/Form1.Designer.cs
@@ -30,11 +30,13 @@
         {
             button1 = new Button();
             lblStatus = new Label();
+            button2 = new Button();
+            lblStatusTAIKIN = new Label();
             SuspendLayout();
             // 
             // button1
             // 
-            button1.Location = new Point(332, 125);
+            button1.Location = new Point(88, 69);
             button1.Name = "button1";
             button1.Size = new Size(94, 29);
             button1.TabIndex = 0;
@@ -46,17 +48,40 @@
             // 
             lblStatus.AutoSize = true;
             lblStatus.Font = new Font("Yu Gothic UI", 12F);
-            lblStatus.Location = new Point(332, 211);
+            lblStatus.Location = new Point(480, 70);
             lblStatus.Name = "lblStatus";
             lblStatus.Size = new Size(72, 28);
             lblStatus.TabIndex = 1;
             lblStatus.Text = "未出勤";
+            // 
+            // button2
+            // 
+            button2.Enabled = false;
+            button2.Location = new Point(88, 234);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 2;
+            button2.Text = "退勤";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // lblStatusTAIKIN
+            // 
+            lblStatusTAIKIN.AutoSize = true;
+            lblStatusTAIKIN.Font = new Font("Yu Gothic UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 128);
+            lblStatusTAIKIN.Location = new Point(480, 238);
+            lblStatusTAIKIN.Name = "lblStatusTAIKIN";
+            lblStatusTAIKIN.Size = new Size(72, 28);
+            lblStatusTAIKIN.TabIndex = 3;
+            lblStatusTAIKIN.Text = "未退勤";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(lblStatusTAIKIN);
+            Controls.Add(button2);
             Controls.Add(lblStatus);
             Controls.Add(button1);
             Name = "Form1";
@@ -69,5 +94,7 @@
 
         private Button button1;
         private Label lblStatus;
+        private Button button2;
+        private Label lblStatusTAIKIN;
     }
 }
--- AttendanceApp/AttendanceApp/Form1.cs
+++ AttendanceApp/AttendanceApp/Form1.cs
@@ -1,3 +1,6 @@
+using System;
+using System.IO;
+
 namespace AttendanceApp
 {
     public partial class Form1 : Form
@@ -9,13 +12,30 @@ namespace AttendanceApp
 
         private void button1_Click(object sender, EventArgs e)　//出勤ボタン
         {
-            string nowTime = DateTime.Now.ToString("HH:mm");
+            string nowTime = DateTime.Now.ToString("HH:mm:ss");
+            File.AppendAllText("attendance_log.txt", nowTime + ",出勤" + Environment.NewLine);
 
-            lblStatus.Text ="出勤済 ("+ nowTime +")";
+            lblStatus.Text = "出勤済み(" + DateTime.Now.ToString("HH:mm") + ")";
 
             button1.Enabled = false;
 
-            MessageBox.Show("打刻完了");
+            MessageBox.Show("打刻完了して記録済み");
+
+            button2.Enabled = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)　//退勤ボタン
+        {
+            string nowTime = DateTime.Now.ToString("HH:mm:ss");
+            File.AppendAllText("attendance_log.txt", nowTime + ",退勤" + Environment.NewLine);
+
+            lblStatusTAIKIN.Text = "退勤済み(" + DateTime.Now.ToString("HH:mm") + ")";
+
+            button2.Enabled = false;
+
+            MessageBox.Show("おつぽん(^_-)-☆");
+
+            button1.Enabled = true;
         }
     }
 }
